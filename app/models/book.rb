140fb365e0c2b2323d262d03bf5c96601878566e
MKiezik <michal.kiezik@gmail.com>
2020-09-27 21:19:11 +0200

    Added validations and the display of validation errors in the new books form

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
-class Book < ApplicationRecord
+class Book < ApplicationRecord  # class Book < ActiveRecord::Base in ROR 4
+    validates :title, :author, :price, :stock_quantity, presence: true
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    validates :stock_quantity, numericality: {only_integer: true}
 end
