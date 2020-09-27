9dea6d5d0e07461bcfe2e73e0c7dd2dd9e86eaa9
MKiezik <michal.kiezik@gmail.com>
2020-09-27 18:28:38 +0200

    Added root route for books index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
     get 'books', to: 'books#index', as: 'books'  # get 'URL', to: 'controller#action', as: 'route_name'
-  
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+
+    root 'books#index'
+
+    # root command
+    # get "/", to: 'books#index', as: 'root'
+
+  end
